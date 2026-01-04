c32b2f27f8c21d9e5aba0505b4b72885ebb988b9
Drew <andrew.stephens.professional@gmail.com>
2026-01-04 01:30:30 +0000

    Add files via upload

---
+++ Renderer.java
@@ -0,0 +1,18 @@
+package flappyBird;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class Renderer extends JPanel{
+
+	private static final long serialVersionID = 1L;
+
+	@Override
+	protected void paintComponent(Graphics g) {
+	
+		super.paintComponent(g);
+		flappyBird.flappyBird.repaint(g);
+	}
+	
+	
+	
+}
